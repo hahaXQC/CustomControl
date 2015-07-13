11360a1ec9d6feabedc4f3eed811f6b9482e6965
rimi <rimi@haha-2.local>
2015-07-13 13:14:31 +0800

    first commit

---
+++ DRvenCustomControl/DRvenCustomControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DRvenCustomControl
+//
+//  Created by rimi on 15/7/13.
+//  Copyright © 2015年 RIMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DRvenCustomControl/DRvenCustomControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DRvenCustomControl
+//
+//  Created by rimi on 15/7/13.
+//  Copyright © 2015年 RIMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
